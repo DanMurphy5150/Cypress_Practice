3ad1eea76b6b1cb9439088f372ffbffe64862769
Daniel Murphy <daniel.murphy@capco.com>
2022-04-17 21:15:32 -0400

    Added a 2nd test and learned about chaining, Cypress behind the scenes, and aliasing

--- CypressAutomation/cypress/integration/examples/Test1.js
+++ CypressAutomation/cypress/integration/examples/Test1.js
@@ -1,14 +1,44 @@
-/// <reference types="Cypress" />
-
+// the line below add the cy intellisense -- need the ///
+///<reference types="Cypress" />
+// Command to open cypress
+// ./node_modules/.bin/cypress open
+//Run a specific test file
+//./node_modules/.bin/cypress run --spec 'cypress/integration/examples/Test2.js'
+// run in specific browser
+//  ./node_modules/.bin/cypress run --browser chrome
 describe('My First Test Suite', function () {
   it('My FirstTest case', function () {
     cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
     cy.get('.search-keyword').type('ca');
     cy.wait(2000);
     cy.get('.product:visible').should('have.length', 4);
+    // parent child chaining
+    cy.get('.products').as('productLocator');
+    cy.get('@productLocator').find('.product').should('have.length', 4);
+    // targets item by index with eg()
+    cy.get('@productLocator')
+      .find('.product')
+      .eq(2)
+      .contains('ADD TO CART')
+      .click();
+    // find product by name
+    cy.get('@productLocator')
+      .find('.product')
+      .each(($el, index, $list) => {
+        const vegetableText = $el.find('h4.product-name').text();
+        if (vegetableText.includes('Cashews')) {
+          $el.find('button').trigger('click');
+        }
+      });
   });
 
-  it('My Second Test Case', function () {
-    // test step
+  it('Logo is displayed correctly', function () {
+    // assert if logo is correctly displayed
+    cy.get('.brand').should('have.text', 'GREENKART');
+
+    // this is to print in the logs
+    cy.get('.brand').then(function (logoElement) {
+      cy.log(logoElement.text());
+    });
   });
 });
