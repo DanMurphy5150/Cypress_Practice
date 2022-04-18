5c50d1824ae67c3474063e090390421a27f2cfc5
Daniel Murphy <daniel.murphy@capco.com>
2022-04-18 19:50:51 -0400

    added more tests

--- CypressAutomation/cypress/integration/examples/Test4.js
+++ CypressAutomation/cypress/integration/examples/Test4.js
@@ -4,5 +4,19 @@ describe('Learning Cypress Automation', function () {
   it('automating Alerts in Cypress', function () {
     cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
     cy.get('#alertbtn').click();
+    //window:alert
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal(
+        'Hello , share this practice page and share your knowledge'
+      );
+    });
+  });
+
+  it('Automating window confirm', function () {
+    cy.on('window:confirm', (str) => {
+      expect(str).to.equal(
+        'Hello , share this practice page and share your knowledge'
+      );
+    });
   });
 });
