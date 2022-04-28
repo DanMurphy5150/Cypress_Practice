40b6889f3a6e86a3e66da50565ce8d0ea1de0088
Daniel Murphy <daniel.murphy@capco.com>
2022-04-28 14:40:01 -0400

    added tests for iframes

--- CypressAutomation/cypress/integration/examples/Test3.js
+++ CypressAutomation/cypress/integration/examples/Test3.js
@@ -9,7 +9,7 @@ describe('Advanced Automation Practice', function () {
       .and('have.value', 'option1');
     cy.get('#checkBoxOption1').uncheck().should('not.be.checked');
     // check all the checkboxes
-    cy.get('input[type="checkbox"').check(['option2', 'option3', 'option1']);
+    cy.get('input[type="checkbox"]').check(['option2', 'option3', 'option1']);
   });
 
   it('Automating Static Dropdowns', function () {
