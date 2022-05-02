a6af3e64166fd53d40bced7e3da58f658a8ad5fa
Daniel Murphy <daniel.murphy@capco.com>
2022-05-02 09:36:58 -0400

    added more angular tests

--- CypressAutomation/cypress/integration/examples/Test9Framework.js
+++ CypressAutomation/cypress/integration/examples/Test9Framework.js
@@ -22,5 +22,15 @@ describe('Setting up Test Hooks', function () {
     cy.visit('https://rahulshettyacademy.com/angularpractice/');
     cy.get('form.ng-untouched > :nth-child(1)').type(this.globalData.name);
     cy.get('select').select(this.globalData.gender);
+    cy.get(':nth-child(4) > .ng-untouched').should(
+      'have.value',
+      this.globalData.name
+    );
+    cy.get("input[name='name']:nth-child(2)").should(
+      'have.attr',
+      'minlength',
+      '2'
+    );
+    cy.get('#inlineRadio3').should('be.disabled');
   });
 });
