91f222bd8e3a1e399e19c77a6fa2384cc47503a1
Daniel Murphy <daniel.murphy@capco.com>
2022-05-04 14:55:38 -0400

    finished an E2E scenario

--- CypressAutomation/cypress/integration/examples/Test9Framework.js
+++ CypressAutomation/cypress/integration/examples/Test9Framework.js
@@ -41,5 +41,22 @@ describe('Setting up Test Hooks', function () {
       console.log(element);
     });
     productsPage.checkoutButton().click();
+    cy.contains('Checkout').click();
+    cy.get('#country').type('United States of America');
+    cy.get('.suggestions > ul > li > a').click();
+    cy.get('#checkbox2').click({ force: true });
+    cy.get('input[type="submit"]').click();
+    // cy.get('.alert').should(
+    //   'have.text',
+    //   'Success! Thank you! Your order will be delivered in next few weeks :-).'
+    // );
+    cy.get('.alert').then(function (element) {
+      const alertText = element.text();
+      expect(
+        alertText.includes(
+          'Success! Thank you! Your order will be delivered in next few weeks :-).'
+        )
+      ).to.be.true;
+    });
   });
 });
