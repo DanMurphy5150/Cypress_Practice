eadc35c5c4a35b0e575a375406f83f5ad20d0b5d
Daniel Murphy <daniel.murphy@capco.com>
2022-05-18 18:42:35 -0400

    More Http tests

--- CypressAutomation/cypress/integration/examples/fakeTest.js
+++ CypressAutomation/cypress/integration/examples/fakeTest.js
@@ -20,5 +20,9 @@ describe('My First Test Suite', function () {
       }
     ).as('bookretrievals');
     cy.get("button[class='btn btn-primary']").click();
+    cy.wait('@bookretrievals').should(({ request, response }) => {
+      cy.get('tr').should('have.length', response.body.length + 1);
+    });
+    cy.get('p').should('have.text', 'Oops only 1 Book available');
   });
 });
